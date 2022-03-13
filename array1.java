7b1ef1c91a52bd520cde6f0c56496bab3eaa4876
Bhavyajha00 <101401527+Bhavyajha00@users.noreply.github.com>
2022-03-13 15:19:14 +0530

    Add files via upload

---
+++ array1.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class array1 {
+    public static void main(String [] args){
+        int[] a = new int[]{45,12,77,34,89,21};
+            Arrays.sort(a);
+            System.out.println(Arrays.toString(a));
+    }
+}
